dac3bf9fd7df30e14fc6d001b2e8727ad272bb19
kimmingehu <13579myskhs@gmail.com>
2026-09-08 19:05:43 +0900

    Update loader.lua

--- games/loader.lua
+++ games/loader.lua
@@ -1,6 +1,6 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
 
-local BASE = 'https://raw.githubusercontent.com/당신의계정명/Ouroboros/main/games/'
+local BASE = 'https://raw.githubusercontent.com/kimmingehu/Ouroboros/main/games/'
 local games = {
     [825735094] = 'stealanegg.lua',
 }
